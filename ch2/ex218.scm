704af66ab1e54f43aad243ec892f1887432328e4
zhangjiji <programmer.zhangjiji@gmail.com>
2012-04-23 00:20:06 +0800

    add ex218

---
+++ ch2/ex218.scm
@@ -0,0 +1,20 @@
+;; exercise 2.18
+
+(define (reverse list1)
+  (if (not (null? list1))
+      (let ((head (car list1))
+	    (tail (cdr list1))
+	    )
+	(if (null? tail)
+	    head
+	    (list (reverse tail) head)
+	)
+      )
+  )
+)
+
+(reverse (list 1 2 3))
+(reverse (reverse (list 1 2 3)))
+
+(car (list (list 1 2) 3))
+(cdr (list (list 1 2) 3))
