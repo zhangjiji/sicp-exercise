6db1cef463ac9aa78dd1922dc06cbdf0b4497bdf
zhe.zhang <zhe.zhang@glu.com>
2012-04-29 02:34:13 +0800

    ch3

--- ch3/3.1.scm
+++ ch3/3.1.scm
@@ -1,2 +1,10 @@
 ;; exercise 3.1.scm
 
+
+(define (make-accumulator init)
+  (lambda (x)
+    (begin (set! init (+ init x)) init)))
+
+(define A (make-accumulator 10))
+(A 10)
+(A 10)
--- ch3/3.2.scm
+++ ch3/3.2.scm
@@ -1,2 +1,16 @@
 ;; exercise 3.2.scm
 
+(define (make-monitored f)
+  (define (reset-count)
+    (begin (set! amount 0) amount))
+  (define (how-many-calls?)
+    (amount)
+  (define (dispatch x)
+    (cond ((eq? x 'how-many-calls?) how-many-calls?)
+	  ((eq? x 'reset-count) reset-count)
+	  (else (f x))))
+  dispatch)
+
+(define s (make-monitored sqrt))
+(s 100)
+(s 'how-many-calls?)
